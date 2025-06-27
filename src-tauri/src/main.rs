3cf8dd66a25932a3d23f27ea578abcf1f7e61680
Rainymy <48211766+Rainymy@users.noreply.github.com>
2025-06-27 17:14:32 +0200

    feat: start on linux

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,11 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-mod types;
+#[cfg(target_os = "windows")]
 mod win32;
 
 fn main() {
+    #[cfg(target_os = "windows")]
     let _ = win32::windows::windows_controller();
     volumize_lib::run();
 }

6b93440e8fe8b554bba7dd4790d97243c62ce0eb
Rainymy <48211766+Rainymy@users.noreply.github.com>
2025-07-17 22:50:42 +0200

    feat: win32 implementation with the new shared API

---
+++ src-tauri/src/win32/volume_controller_trait.rs
@@ -0,0 +1,60 @@
+use super::VolumeController;
+use crate::types::shared::{
+    AppIdentifier, ApplicationVolumeControl, AudioSession, DeviceControl, MasterVolumeControl,
+    VolumeControllerTrait, VolumePercent, VolumeResult,
+};
+
+impl MasterVolumeControl for VolumeController {
+    fn get_master_volume(&self) -> VolumeResult<Option<VolumePercent>> {
+        self.with_default_audio_endpoint(|endpoint| unsafe {
+            let volume = endpoint.GetMasterVolumeLevelScalar()?;
+            Ok(Some(volume))
+        })
+    }
+
+    fn set_master_volume(&self, _percent: VolumePercent) -> VolumeResult<()> {
+        Ok(())
+    }
+
+    fn mute_master(&self) -> VolumeResult<()> {
+        Ok(())
+    }
+
+    fn unmute_master(&self) -> VolumeResult<()> {
+        Ok(())
+    }
+}
+
+impl ApplicationVolumeControl for VolumeController {
+    fn set_app_volume(&self, _app: AppIdentifier, _percent: VolumePercent) -> VolumeResult<()> {
+        Ok(())
+    }
+
+    fn mute_app(&self, _app: AppIdentifier) -> VolumeResult<()> {
+        Ok(())
+    }
+
+    fn unmute_app(&self, _app: AppIdentifier) -> VolumeResult<()> {
+        Ok(())
+    }
+
+    fn get_all_applications(&self) -> VolumeResult<Vec<AudioSession>> {
+        Ok(vec![])
+    }
+}
+
+impl DeviceControl for VolumeController {
+    fn get_playback_devices(&self) -> VolumeResult<Vec<AudioSession>> {
+        Ok(vec![])
+    }
+
+    fn get_current_playback_device(&self) -> VolumeResult<Option<AudioSession>> {
+        Ok(None)
+    }
+}
+
+impl VolumeControllerTrait for VolumeController {
+    fn load_sessions(&self) -> VolumeResult<Vec<AudioSession>> {
+        Ok(vec![])
+    }
+}
--- src-tauri/src/win32/windows.rs
+++ src-tauri/src/win32/windows.rs
@@ -1,29 +1,14 @@
 use windows::Win32::{
     // Foundation::HWND,
     Media::Audio::{Endpoints::IAudioEndpointVolume, *},
+    System::Com::CLSCTX_ALL,
 };
 
+use crate::types::shared::VolumeControllerTrait;
 use crate::types::shared::VolumeResult;
-use crate::types::shared::{AppIdentifier, AudioSession, VolumeControllerTrait, VolumePercent};
+
 mod com_scope;
-
-pub fn windows_controller() -> VolumeResult<Vec<AudioSession>> {
-    return com_scope::with_com_initialized(|device_enumerator| unsafe {
-        // Options: eMultimedia - eConsole;
-        let _default_device = device_enumerator.GetDefaultAudioEndpoint(eRender, eConsole)?;
-
-        let hello: IAudioEndpointVolume =
-            _default_device.Activate(windows::Win32::System::Com::CLSCTX_ALL, None)?;
-
-        // GetMasterVolumeLevelScalar = volume %
-        // GetMasterVolumeLevel       = volume DB
-        let world = hello.GetMasterVolumeLevelScalar().unwrap();
-
-        dbg!(world);
-
-        Ok(vec![])
-    });
-}
+mod volume_controller_trait;
 
 pub struct VolumeController;
 
@@ -31,48 +16,27 @@ pub fn make_controller() -> Box<dyn VolumeControllerTrait> {
     return Box::new(VolumeController);
 }
 
-impl VolumeControllerTrait for VolumeController {
-    fn get_playback_devices(&self) -> VolumeResult<Vec<AudioSession>> {
-        return windows_controller();
+impl VolumeController {
+    fn with_default_audio_endpoint<F, R>(&self, callback: F) -> VolumeResult<R>
+    where
+        F: FnOnce(&IAudioEndpointVolume) -> VolumeResult<R>,
+    {
+        com_scope::with_com_initialized(|device_enumerator| unsafe {
+            let default_device = device_enumerator.GetDefaultAudioEndpoint(eRender, eConsole)?;
+            let endpoint_volume: IAudioEndpointVolume =
+                default_device.Activate(CLSCTX_ALL, None)?;
+            callback(&endpoint_volume)
+        })
     }
-
-    fn get_current_playback_device(&self) -> VolumeResult<Option<AudioSession>> {
-        Ok(None)
-    }
-
-    fn get_master_volume(&self) -> VolumeResult<Option<VolumePercent>> {
-        Ok(Some(0.8))
-    }
-
-    fn set_master_volume(&self, _percent: VolumePercent) -> VolumeResult<()> {
-        Ok(())
-    }
-
-    fn mute_master(&self) -> VolumeResult<()> {
-        Ok(())
-    }
-
-    fn unmute_master(&self) -> VolumeResult<()> {
-        Ok(())
-    }
-
-    fn set_app_volume(&self, _app: AppIdentifier, _percent: VolumePercent) -> VolumeResult<()> {
-        Ok(())
-    }
-
-    fn mute_app(&self, _app: AppIdentifier) -> VolumeResult<()> {
-        Ok(())
-    }
-
-    fn unmute_app(&self, _app: AppIdentifier) -> VolumeResult<()> {
-        Ok(())
-    }
-
-    fn load_sessions(&self) -> VolumeResult<Vec<AudioSession>> {
-        Ok(vec![])
-    }
-
-    fn get_all_applications(&self) -> VolumeResult<Vec<AudioSession>> {
-        Ok(vec![])
+    fn _with_default_audio_sessions<F, R>(&self, callback: F) -> VolumeResult<R>
+    where
+        F: FnOnce(&IAudioSessionManager2) -> VolumeResult<R>,
+    {
+        com_scope::with_com_initialized(|device_enumerator| unsafe {
+            let default_device = device_enumerator.GetDefaultAudioEndpoint(eRender, eConsole)?;
+            let session_manager: IAudioSessionManager2 =
+                default_device.Activate(CLSCTX_ALL, None)?;
+            callback(&session_manager)
+        })
     }
 }

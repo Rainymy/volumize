c9d68b5a0288bf29b1446b1aa6d31f20cd748f3e
Rainymy <48211766+Rainymy@users.noreply.github.com>
2025-09-17 10:27:03 +0200

    feat: retry and connect

--- src/App.tsx
+++ src/App.tsx
@@ -10,3 +10,42 @@ export default function App() {
         </Container>
     );
 }
+
+class ConnectSocket {
+    socket: WebSocket | null = null;
+
+    async retryUntilConnection(): Promise<WebSocket | null> {
+        let index = 1;
+        while (index <= 5) {
+            try {
+                console.log("Connecting...", index++);
+                await this.retryDelay();
+                return await this.__connect__();
+            } catch {
+                console.log("Failed to connect retying", index);
+            }
+        }
+
+        return null
+    }
+
+    async retryDelay() {
+        return new Promise((resolve) => setTimeout(resolve, 1000));
+    }
+
+    __connect__(): Promise<WebSocket> {
+        return new Promise((resolve, reject) => {
+            const socket = new WebSocket("ws://localhost:3000");
+
+            socket.onerror = (_event) => {
+                reject();
+            };
+
+            socket.onopen = (_event) => {
+                resolve(socket);
+            };
+        });
+    }
+}
+
+new ConnectSocket().retryUntilConnection();

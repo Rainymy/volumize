5888a46952e33a31b89ca26f029ce38ed882f665
Rainymy <48211766+Rainymy@users.noreply.github.com>
2025-09-23 01:18:30 +0200

    feat: platform detection

---
+++ src/bridge/generic.ts
@@ -0,0 +1,11 @@
+import { platform } from "@tauri-apps/plugin-os";
+
+export function is_desktop() {
+    switch (platform()) {
+        case "android":
+        case "ios":
+            return false
+        default:
+            return true;
+    }
+}

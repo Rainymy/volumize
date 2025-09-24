bd842049b9c979f345c22bd5dc5721b868b6620d
Rainymy <48211766+Rainymy@users.noreply.github.com>
2025-09-24 22:05:30 +0200

    fix: URL & PORT update

--- src/components/audioMixer/index.tsx
+++ src/components/audioMixer/index.tsx
@@ -1,4 +1,4 @@
-import { useAtom } from "jotai";
+import { useAtom, useAtomValue } from "jotai";
 import { useEffect, useState } from "react";
 import { volumeController } from "$bridge/volumeManager";
 import { WebsocketTauriVolumeController } from "$bridge/websocket_volume";
@@ -9,31 +9,39 @@ import { getNumber } from "$util/generic";
 import wrapper from "./index.module.less";
 
 export function AudioMixer() {
-    const [connect_url, set_connect_url] = useAtom(server_url);
-    const [connect_port, set_connect_port] = useAtom(server_port);
+    const connect_url = useAtomValue(server_url);
+    const connect_port = useAtomValue(server_port);
     const [is_ready, set_is_ready] = useAtom(connection_ready);
 
     useEffect(() => {
+        let cancelled = false;
         (async () => {
             if (volumeController instanceof WebsocketTauriVolumeController) {
-                await volumeController.setup(connect_url, connect_port);
-                set_is_ready(true);
+                await volumeController.setup(connect_url, connect_port).catch(error => console.log(error));
+                if (!cancelled) {
+                    set_is_ready(true);
+                }
             }
         })();
-
         return () => {
+            cancelled = true;
             if (volumeController instanceof WebsocketTauriVolumeController) {
                 volumeController.close();
+                set_is_ready(false);
             }
-            set_is_ready(false);
         };
     }, [connect_url, connect_port, set_is_ready]);
 
     return (
         <div className={wrapper.container}>
-            {!is_ready && <ServerURLComponent />}
-            <Sidebar />
-            <MainContent />
+            {!is_ready ? (
+                <ServerURLComponent />
+            ) : (
+                <>
+                    <Sidebar />
+                    <MainContent />
+                </>
+            )}
         </div>
     );
 }
@@ -50,7 +58,6 @@ function ServerURLComponent() {
         const url = formData.get("url")?.toString();
         const port = formData.get("port")?.toString();
 
-        console.log("info: ", url, port);
         if (!url) {
             return setErrorText("Invalid. URL address!");
         }
@@ -64,8 +71,10 @@ function ServerURLComponent() {
             return setErrorText("Invalid. PORT address range!");
         }
 
-        set_connect_url(url);
-        set_connect_port(port_number);
+        console.log("Setting new ULR and PORT address....");
+        console.log("info: ", url, port);
+        set_connect_url(() => url);
+        set_connect_port(() => port_number);
     }
 
     return (

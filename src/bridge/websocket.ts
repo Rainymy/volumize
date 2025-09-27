be91b4782887bc4719a6e569e4cdbefa00e8aaf4
Rainymy <48211766+Rainymy@users.noreply.github.com>
2025-09-27 03:37:05 +0200

    feat: simplify websocket (frontend)

--- src/bridge/websocket.ts
+++ src/bridge/websocket.ts
@@ -1,75 +1,23 @@
+import WebSocket from "@tauri-apps/plugin-websocket";
+
 export class ConnectSocket {
     public socket: WebSocket | null = null;
-    private connect_URL: URL = new URL("ws://localhost:9001");
-    private cancelResolve: (() => void) | null = null;
+    private connect_URL: string = "ws://localhost:9001";
 
     setup(url: string, port: number) {
-        this.connect_URL = new URL(`ws://${url}:${port}`);
+        this.connect_URL = `ws://${url}:${port}`;
     }
 
-    async retryUntilConnection(): Promise<ConnectSocket | null> {
-        const cancelPromise = new Promise((_, reject) => {
-            this.cancelResolve = () => reject(new Error("Cancelled"));
-        });
-
-        let index = 0;
-        while (index < 5) {
-            try {
-                console.log("Connecting...", ++index);
-
-                await Promise.race([this.retryDelay(), cancelPromise]);
-                await Promise.race([this.__connect__(), cancelPromise]);
-
-                console.log("new connection");
-                return this;
-            } catch (error) {
-                if ((error as Error)?.message === "Cancelled") {
-                    console.log("Connection cancelled");
-                    return null;
-                }
-                else {
-                    console.log("Failed to connect retying", index);
-                }
-            }
-        }
-
-        console.warn("Connection failed");
-        return null;
-    }
-
-    async retryDelay() {
-        return new Promise<void>((resolve) => setTimeout(resolve, 1000));
-    }
-
-    close() {
-        this.cancelResolve?.();
-        this.socket?.close();
+    async close() {
+        await this.socket?.disconnect();
         this.socket = null;
     }
 
-    async __connect__() {
-        return new Promise<void>((resolve, reject) => {
-            if (!this.socket) {
-                const socket = new WebSocket(this.connect_URL);
+    async connect() {
+        this.socket = await WebSocket.connect(this.connect_URL);
+    }
 
-                socket.onerror = (_event) => {
-                    this.socket = null;
-                    reject();
-                };
-
-                socket.onopen = (_event) => {
-                    this.socket = socket;
-                    resolve();
-                };
-
-                return;
-            }
-
-            if (this.socket.readyState === this.socket.OPEN) {
-                return resolve();
-            }
-
-            reject(new Error("Socket not ready"));
-        });
+    async send(data: string) {
+        this.socket?.send({ type: "Text", data: data })
     }
 }

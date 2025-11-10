11dabe5c8da2375711542e397d39b6517c0e3c4b
Rainymy <48211766+Rainymy@users.noreply.github.com>
2025-11-10 05:18:42 +0100

    feat: setup discovery component

--- src/components/serverInput/index.tsx
+++ src/components/serverInput/index.tsx
@@ -1,22 +1,56 @@
 import { useAtom } from "jotai";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import { FaSearch } from "react-icons/fa";
 
 import { AppButton } from "$base/button";
 import { AppInput } from "$base/input";
-import { server_port, server_url } from "$model/volume";
+import { volumeController } from "$bridge/volumeManager";
+import { WebsocketTauriVolumeController } from "$bridge/websocket_volume";
+import { server_port, server_url } from "$model/server_url";
+import { connection_ready as connection_status } from "$model/volume";
 import { PORT } from "$type/globals";
 import { getNumber } from "$util/generic";
+
 import style from "./index.module.less";
 
 export function ServerURLComponent() {
+    // use to track if component is unmounted.
+    // while getting server url.
+    const ref = useRef<boolean>(false);
+    const [_connection_ready, set_connection_ready] = useAtom(connection_status);
+
+    async function discoverServer() {
+        // Implement server discovery logic here
+        if (volumeController instanceof WebsocketTauriVolumeController) {
+            set_connection_ready(() => false);
+            const server = await volumeController.discoverServer();
+
+            if (!server) {
+                // Show error message
+                return;
+            }
+
+            if (!ref.current) {
+                await volumeController.close();
+                await volumeController.setup(server.url, server.port);
+                set_connection_ready(() => true);
+            }
+        }
+    }
+
+    useEffect(() => {
+        () => {
+            ref.current = true;
+        };
+    }, []);
+
     return (
         <div className={style.box}>
             <ServerInput />
             <hr />
             <div className={style.discover_server}>
-                <AppButton>Discover Servers</AppButton>
+                <AppButton onClick={discoverServer}>Discover Servers</AppButton>
             </div>
         </div>
     );
--- src/utils/fake.ts
+++ src/utils/fake.ts
@@ -1,11 +1,4 @@
-import {
-    type AudioApplication,
-    type AudioDevice,
-    type AudioSession,
-    SessionDirection,
-    SessionType,
-    type VolumePercent,
-} from "$type/volume";
+import { type AudioDevice, SessionDirection, type VolumePercent } from "$type/volume";
 
 export function generate_random_device(): AudioDevice {
     const id = Math.random().toString(36).substring(2, 8);
@@ -24,35 +17,5 @@ export function generate_random_device(): AudioDevice {
         friendly_name,
         volume,
         is_default,
-    };
-}
-
-export function generate_random_application(): AudioApplication {
-    const id = Math.floor(Math.random() * 10_000);
-
-    const volume = {
-        current: Math.random() as VolumePercent,
-        muted: Math.random() < 0.25,
-    };
-
-    return {
-        process: {
-            id,
-            name: `Application ${id}`,
-            path: null,
-        },
-        direction: SessionDirection.Render,
-        session_type: SessionType.Application,
-        sound_playing: Math.random() < 0.75,
-        volume,
-    };
-}
-
-export function generate_random_session(count: number): AudioSession {
-    return {
-        device: generate_random_device(),
-        applications: [
-            ...Array.from({ length: count }, () => generate_random_application()),
-        ],
     };
 }

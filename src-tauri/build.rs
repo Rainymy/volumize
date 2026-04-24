b1856dcaa4359ac9e6d0d688d57a449a7393810e
Rainymy <48211766+Rainymy@users.noreply.github.com>
2026-04-24 18:43:20 +0200

    rename helper file name

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -13,7 +13,7 @@ fn main() {
         }
 
         let helper_folder = "./helper-win32";
-        let helper_file = format!("{}/target/release/helper-win32.exe", helper_folder);
+        let helper_file = format!("{}/target/release/helper.exe", helper_folder);
         let helper_path = PathBuf::from(&helper_file);
 
         let status = Command::new("cargo")
@@ -23,12 +23,13 @@ fn main() {
                 "--manifest-path",
                 &format!("{}/Cargo.toml", helper_folder),
             ])
+            // .env("EMBEDDED_WIN32_SHA256", "<Just setting here>")
             .status()
             .expect("Failed to build helper-win32");
 
         assert!(status.success(), "helper-win32 build failed");
 
-        let result = build_helper::sign::sign_binary(&helper_path);
+        let result = build_helper::sign::sign_binary();
         for error in result.errors {
             println!("cargo:warning=[sign binary]: {}", error);
         }

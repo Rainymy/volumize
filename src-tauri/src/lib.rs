beb2cf02965565b1c5bb3f5437b3060ffe75260f
Rainymy <48211766+Rainymy@users.noreply.github.com>
2026-05-06 00:11:25 +0200

    debug: setup panic hooks for printing error on mobile

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -7,6 +7,12 @@ mod types;
 /// Entry point for the android/ios application.
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn start_application() {
+    std::panic::set_hook(Box::new(|info| {
+        // This will appear in adb logcat before the abort
+        eprintln!("TAURI PANIC: {}", info);
+        std::thread::sleep(std::time::Duration::from_millis(500));
+    }));
+
     tauri::Builder::default()
         .plugin(tauri_plugin_websocket::init())
         .plugin(tauri_plugin_os::init())
